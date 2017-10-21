e93cd957f2e831dabce56aaa74954df46cab3628
Dominic Barnes <dominic@dbarnes.info>
2017-10-21 10:36:03 -0700

    feat(server): add favicon and logs

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 
 const express = require('express')
+const favicon = require('serve-favicon')
 const graphql = require('express-graphql')
 const Loki = require('lokijs')
+const morgan = require('morgan')
 const path = require('path')
-const schema = require('./lib/schema')
-const port = process.env.PORT || 4000
 
+const schema = require('./lib/schema')
+
+const port = process.env.PORT || 4000
 const app = express()
 const db = new Loki(path.resolve(__dirname, './data/game-master.db'))
+
+app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')))
+app.use(morgan('tiny'))
 
 db.loadDatabase(null, err => {
   if (err) throw err
